6d75787fff11844c9f3ff461034c99c9538a2817
LISTERINE <jferretti6890@gmail.com>
2016-02-16 09:06:42 -0500

    Removed most of the code because I realized it was about 90% bloat. Working with pad! still need more tests before I call it good.

--- ddr_pad_controller.ino
+++ ddr_pad_controller.ino
@@ -29,46 +29,30 @@ Bounce button14 = Bounce(14, 10);
 Bounce button15 = Bounce(15, 10);
 */
 
-boolean pad_0_Fall = false; // up
-boolean pad_1_Fall = false; // left
-boolean pad_2_Fall = false; // down
-boolean pad_3_Fall = false; //right
+boolean pad_up_pressed = false;
+boolean pad_left_pressed = false;
+boolean pad_down_Fall = false;
+boolean pad_right_Fall = false;
 
-boolean rightPressed_0 = false;
-  boolean rightPressed_1 = false;
-  boolean rightPressed_2 = false;
-  boolean rightPressed_3 = false;
-  boolean leftPressed_0= false;
-  boolean leftPressed_1 = false;
-  boolean leftPressed_2 = false;
-  boolean leftPressed_3 = false;
-  boolean upPressed_0 = false;
-  boolean upPressed_1 = false;
-  boolean upPressed_2 = false;
-  boolean upPressed_3 = false;
-  boolean downPressed_0 = false;
-  boolean downPressed_1 = false;
-  boolean downPressed_2 = false;
-  boolean downPressed_3 = false;
   
 
 void setup() {
-  pinMode(0, INPUT);
-  pinMode(1, INPUT);
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
-  pinMode(5, INPUT);
-  pinMode(6, INPUT);  // Teensy++ LED, may need 1k resistor pullup
-  pinMode(7, INPUT);
-  pinMode(8, INPUT);
-  pinMode(9, INPUT);
-  pinMode(10, INPUT);
-  pinMode(11, INPUT);
-  pinMode(12, INPUT);
-  pinMode(13, INPUT);
-  pinMode(14, INPUT);
-  pinMode(15, INPUT);
+  pinMode(0, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP); // skip over 12/13. Teensy++ LED, may need 1k resistor pullup. just move to 14 to avoid extra soldering. skip 12 to keep wires grouped.
+  pinMode(15, INPUT_PULLUP);
+  pinMode(16, INPUT_PULLUP);
+  pinMode(17, INPUT_PULLUP);
 
 
 }
@@ -102,180 +86,39 @@ void loop() {
    ******************************************************/
 
    
-  // Check pad 0
-  if ((!upPressed_0) || (!upPressed_1) || (!upPressed_2) || (!upPressed_3)) { // If no buttons(sensors) are pressed
-    if (digitalRead(0) == HIGH) {                                              // Check if button was pressed, if it was, set this button and this pad as pressed
-      upPressed_0 = true;
-      pad_0_Fall = true;
-    }
-    if (digitalRead(1) == HIGH) {
-      upPressed_1 = true;
-      pad_0_Fall = true;
-    }
-    if (digitalRead(2) == HIGH) {
-      upPressed_2 = true;
-      pad_0_Fall = true;
-    }
-    if (digitalRead(3) == HIGH) {
-      upPressed_3 = true;
-      pad_0_Fall = true;
-    }
-  }
-  else {                                                                       // If there are pressed buttons, check if any have come up
-    if (digitalRead(0) == LOW) {                                          
-      upPressed_0 = false;
-    }
-    if (digitalRead(1) == LOW) {
-      upPressed_1 = false;
-    }
-    if (digitalRead(2) == LOW) {
-      upPressed_2 = false;
-    }
-    if (digitalRead(3) == LOW) {
-      upPressed_3 = false;
-    }
-  }
-
-  // Check pad 1
-  if ((!leftPressed_0) || (!leftPressed_1) || (!leftPressed_2) || (!leftPressed_3)) {
-    if (digitalRead(4) == HIGH) {
-      leftPressed_0 = true;
-      pad_1_Fall = true;
-    }
-    if (digitalRead(5) == HIGH) {
-      leftPressed_1 = true;
-      pad_1_Fall = true;
-    }
-    if (digitalRead(6) == HIGH) {
-      leftPressed_2 = true;
-      pad_1_Fall = true;
-    }
-    if (digitalRead(7) == HIGH) {
-      leftPressed_3 = true;
-      pad_1_Fall = true;
-    } 
+  // Check pad up
+  if ((digitalRead(0) == LOW) || (digitalRead(1) == LOW) || (digitalRead(2) == LOW) || (digitalRead(3) == LOW)) {
+    pad_up_pressed = true;
+    Keyboard.set_key1(KEY_W);
   }
   else {
-    if (digitalRead(4) == LOW) {
-      leftPressed_0 = false;
-    }
-    if (digitalRead(5) == LOW) {
-      leftPressed_1 = false;
-    }
-    if (digitalRead(6) == LOW) {
-      leftPressed_2 = false;
-    }
-    if (digitalRead(7) == LOW) {
-      leftPressed_3 = false;
-    }
+    pad_up_pressed = false;
+    Keyboard.set_key1(0); // Release
   }
 
-  // Check pad 2
-  if ((!downPressed_0) || (!downPressed_1) || (!downPressed_2) || (!downPressed_3)) {
-    if (digitalRead(8) == HIGH) {
-      downPressed_0 = true;
-      pad_2_Fall = true;
-    }
-    if (digitalRead(9) == HIGH) {
-      downPressed_1 = true;
-      pad_2_Fall = true;
-    }
-    if (digitalRead(10) == HIGH) {
-      downPressed_2 = true;
-      pad_2_Fall = true;
-    }
-    if (digitalRead(11) == HIGH) {
-      downPressed_3 = true;
-      pad_2_Fall = true;
-    }
+  // Check pad left
+  if ((digitalRead(4) == LOW) || (digitalRead(5) == LOW) || (digitalRead(6) == LOW) || (digitalRead(7) == LOW)) {
+    Keyboard.set_key2(KEY_A);
   }
   else {
-    if (digitalRead(8) == LOW) {
-      downPressed_0 = false;
-    }
-    if (digitalRead(9) == LOW) {
-      downPressed_1 = false;
-    }
-    if (digitalRead(10) == LOW) {
-      downPressed_2 = false;
-    }
-    if (digitalRead(11) == LOW) {
-      downPressed_3 = false;
-    }
+    Keyboard.set_key2(0); // Release
   }
 
-  // Check pad 3
-  if ((!rightPressed_0) || (!rightPressed_1) || (!rightPressed_2) || (!rightPressed_3)) {
-    if (digitalRead(12) == HIGH) {
-      rightPressed_0 = true;
-      pad_3_Fall = true;
-    }
-    if (digitalRead(13) == HIGH) {
-      rightPressed_1 = true;
-      pad_3_Fall = true;
-    }
-    if (digitalRead(14) == HIGH) {
-      rightPressed_2 = true;
-      pad_3_Fall = true;
-    }
-    if (digitalRead(15) == HIGH) {
-      rightPressed_3 = true;
-      pad_3_Fall = true;
-    }
+  // Check pad down
+  if ((digitalRead(8) == LOW) || (digitalRead(9) == LOW) || (digitalRead(10) == LOW) || (digitalRead(11) == LOW)) {
+    Keyboard.set_key3(KEY_S);
   }
   else {
-    if (digitalRead(12) == LOW) {
-      downPressed_0 = false;
-    }
-    if (digitalRead(13) == LOW) {
-      downPressed_1 = false;
-    }
-    if (digitalRead(14) == LOW) {
-      downPressed_2 = false;
-    }
-    if (digitalRead(15) == LOW) {
-      downPressed_3 = false;
-    }
+    Keyboard.set_key3(0); // Release
   }
 
-  // If no sensors are pressed
-  if ((!upPressed_0) && (!upPressed_1) && (!upPressed_2) && (!upPressed_3)) {
-    Keyboard.release('w');
+  // Check pad right
+  if ((digitalRead(14) == LOW) || (digitalRead(15) == LOW) || (digitalRead(16) == LOW) || (digitalRead(17) == LOW)) {
+    Keyboard.set_key4(KEY_D);
   }
-  else if (pad_0_Fall) { // if there was a state change in the pad
-    if ((upPressed_0) || (upPressed_1) || (upPressed_2) || (upPressed_3)) {
-      Keyboard.press('w');
-    }
-  }
-  if ((!leftPressed_0) && (!leftPressed_1) && (!leftPressed_2) && (!leftPressed_3)) {
-    Keyboard.release('a');
-  }
-  else if (pad_1_Fall) {
-    if ((leftPressed_0) || (leftPressed_1) || (leftPressed_2) || (leftPressed_3)) {
-      Keyboard.press('a');
-    }
-  }
-  if ((!downPressed_0) && (!downPressed_1) && (!downPressed_2) && (!downPressed_3)) {
-    Keyboard.release('s');
-  }
-  else if (pad_2_Fall) {
-    if ((downPressed_0) || (downPressed_1) || (downPressed_2) || (downPressed_3)) {
-      Keyboard.press('s');
-    }
-  }
-  if ((!rightPressed_0) && (!rightPressed_1) && (!rightPressed_2) && (!rightPressed_3)) {
-    Keyboard.release('d');
-  }
-  else if (pad_3_Fall) {
-    if ((rightPressed_0) || (rightPressed_1) || (rightPressed_2) || (rightPressed_3)) {
-      Keyboard.press('d');
-    }
+  else {
+    Keyboard.set_key4(0); // Release
   }
   
-  pad_0_Fall = false;
-  pad_1_Fall = false;
-  pad_2_Fall = false;
-  pad_3_Fall = false;
-  
+  Keyboard.send_now(); // Send the keys that have been set
 }
-
